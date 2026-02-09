603beadcbf4fbad75bb511bb12fd0715e038051f
Hitanshu Goyal <hitanshugoyal1724@gmail.com>
2026-02-09 23:40:28 +0530

    Time: 21 ms (75.09%), Space: 50.6 MB (12.51%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -25,14 +25,33 @@ class Solution {
             temp = temp.next;
         }
         int[] res = new int[size];
-        for(int k = 0;k < size;k++){
-            res[k] = 0;
-            for(int j = k+1;j < size;j++){
-                if(arr[j] > arr[k]){
-                    res[k] = arr[j];
-                    break;
-                }
+        // THIS IS THE SOLUTION USING TWO FOR LOOPS - O(N^2) TC
+
+        // for(int k = 0;k < size;k++){
+        //     res[k] = 0;
+        //     for(int j = k+1;j < size;j++){
+        //         if(arr[j] > arr[k]){
+        //             res[k] = arr[j];
+        //             break;
+        //         }
+        //     }
+        // }
+
+        //NOW WE WILL SOLVE THIS USING STACK
+        Stack<Integer> st = new Stack<>();
+        res[size-1] = 0;
+        st.push(arr[size-1]);
+        for(int j = size-2;j >=0;j--){
+            while(st.size() > 0 && arr[j] >= st.peek()){
+                st.pop();
             }
+            if(st.size() == 0){
+                res[j] = 0;
+            }
+            else{
+                res[j] = st.peek();
+            }
+            st.push(arr[j]);
         }
         return res;
     }
